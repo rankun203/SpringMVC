a453e1aa760f542e7a99012e7030694d8401251b
Kun Ran <rankun203@gmail.com>
2013-10-26 21:50:47 +0800

    Hello World

---
+++ src/main/java/org/rankun/learn/spring/controller/GreetingController.java
@@ -0,0 +1,30 @@
+package org.rankun.learn.spring.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.rankun.learn.spring.models.Greeting;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 坤
+ * Date: 13-10-26
+ * Time: 下午4:20
+ */
+@Controller
+public class GreetingController {
+    private final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("greeting")
+	public @ResponseBody
+	Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        Logger.getGlobal().log(Level.INFO, "greeting executing.");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<h1>Welcome, Please accepts my <a href="greeting">greeting</a></h1>
+</body>
+</html>
